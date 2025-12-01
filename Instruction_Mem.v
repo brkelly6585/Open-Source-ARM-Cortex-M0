25e12646ed14e29d16730e8da5ac739c6b35c7ee
Bryce Kelly <brkelly6585@gmail.com>
2025-12-01 13:42:30 -0800

    Updated instructions
    Only uploading the instruction mem with more data instructions for now. Unable to upload memory and branch operations since the current implementation causes cycle issues that are impacting data instructions. Leaving this as current working build.

--- Instruction_Mem.v
+++ Instruction_Mem.v
@@ -40,10 +40,21 @@ module Instruction_Mem(
         //Instructions go here
         Instructions[0] = 16'b00100_000_00000000; // MOV (Immediate) | MOVS R0, #0
         Instructions[1] = 16'b00100_001_00000001; // MOV (Immediate) | MOVS R1, #1
-        Instructions[2] = 16'b010001000_0000_001; // ADD (Reg) | ADD R0, R1
+        Instructions[2] = 16'b01000100_0_0001_000; // ADD (Reg) | ADD R0, R1
         Instructions[3] = 16'b0100000000_001_000; // ANDS | ANDS R0, R1
+        //Instructions[4] = 16'b01001_011_00000001; // LDR (Lit) | LDR R3, 1
+        //Instructions[5] = 16'b10010_001_00000001; // STR (Imm) | STR R1, 1
         Instructions[4] = 16'b0100001111_001_110; // MVNS | MVNS R6, R1
         Instructions[5] = 16'b00010_00001_110_111; // ASRS (imm) | ASRS R7, R6, #1
+        Instructions[6] = 16'b00100_010_11110000; // MOV (Immediate) | MOVS R2, #240
+        Instructions[7] = 16'b0100001100_001_010; // ORRS | ORRS R2, R1
+        Instructions[8] = 16'b0100001110_001_010; // BICS | BICS R2, R1
+        Instructions[9] = 16'b0100000001_001_000; // EORS | EORS R0, R1
+        Instructions[10] = 16'b00000_00011_001_100; // LSLS (imm) | LSLS R4, R1, #3
+        Instructions[11] = 16'b00001_00010_100_101; // LSRS (imm) | LSRS R5, R4, #2
+        Instructions[12] = 16'b0100001101_100_101; // MULS | MULS R5, R4
+        Instructions[13] = 16'b0001101_001_101_011; // SUBS (reg) | SUBS R3, R5, R1
+        Instructions[14] = 16'b0100001001_011_100; // RSBS | RSBS R4, R3, #0
         
     end
     
